3919c618de99d5d8e370d2667536ae5efcaa0246
Anchal Kapse <60254026+anchal-kapse@users.noreply.github.com>
2022-07-18 13:35:13 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -2,6 +2,6 @@ function Table() {
     let num = prompt("Enter Number")
     for (let i = 1; i <= 10; i++) {
         var j = i * num
-        console.log(`${num} * ${i} = ${j}`)
+        console.log(num, '*', i, '=', j)
     }
 }
